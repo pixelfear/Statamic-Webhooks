8b13d1b1e945829fba168f6914c8390d09791749
SergeiF <s@sergeif.me>
2014-03-29 08:11:31 +1300

    changes spacing from spaces to tabs

--- _add-ons/webhooks/hooks.webhooks.php
+++ _add-ons/webhooks/hooks.webhooks.php
@@ -3,29 +3,29 @@
 class Hooks_webhooks extends Hooks
 {
 
-    public function webhooks__go()
-    {
-        if (array_get($this->config, 'clear_cache', true)) {
-            $this->clearCache();
-        }
-        // clear OpCache PHP cache storage installed as part of PHP5.5.*
-        if (function_exists('opcache_reset')) {
-            $this->clearOpCache();
-        }
+	public function webhooks__go()
+	{
+		if (array_get($this->config, 'clear_cache', true)) {
+			$this->clearCache();
+		}
+		// clear OpCache PHP cache storage installed as part of PHP5.5.*
+		if (function_exists('opcache_reset')) {
+			$this->clearOpCache();
+		}
 
-    }
+	}
 
-    private function clearCache()
-    {
-        $cache_folder = BASE_PATH . '/_cache/_app/';
-        Folder::delete($cache_folder, true);
-        $this->log->info('The cache has been cleared.');
-    }
+	private function clearCache()
+	{
+		$cache_folder = BASE_PATH . '/_cache/_app/';
+		Folder::delete($cache_folder, true);
+		$this->log->info('The cache has been cleared.');
+	}
 
-    private function clearOpCache()
-    {
-        opcache_reset();
-        $this->log->info('OpCache has been cleared.');
-    }
+	private function clearOpCache()
+	{
+		opcache_reset();
+		$this->log->info('OpCache has been cleared.');
+	}
 
 }
